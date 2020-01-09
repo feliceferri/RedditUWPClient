d4c5fbba20cc17c618f7712010e593ba2b51f4b3
Felice Ferri <feliceferri@gmail.com>
2020-01-09 07:40:04 -0400

    Reponsive Portrait/Landscape

--- RedditUWPClient/MainPage.xaml.cs
+++ RedditUWPClient/MainPage.xaml.cs
@@ -25,11 +25,42 @@ namespace RedditUWPClient
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+       
         public MainPage()
         {
             this.InitializeComponent();
 
+            this.SizeChanged += MainPage_SizeChanged;
         }
 
+        private void MainPage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+           
+            double borderStars = 0.4; //default
+
+            if(this.ActualHeight> this.ActualWidth)
+            {
+                borderStars = 0.02;
+            }
+            else
+            {
+                double ratio = this.ActualWidth / this.ActualHeight;
+                
+                if(ratio < 2)
+                {
+                    borderStars = ratio % 1;  //Remainder of the division, came to this after having a large switch case with different ratios
+                }
+                else
+                {
+                    borderStars = 1;
+                }
+                
+            }
+
+            this.MaingGrid_LeftColumnDefinition.Width = new GridLength(borderStars, GridUnitType.Star);
+            this.MaingGrid_RightColumnDefinition.Width = new GridLength(borderStars, GridUnitType.Star);
+        }
     }
 }
