364577b1dd1cf9165afdb284e4a178a7310cbc4a
Felice Ferri <feliceferri@gmail.com>
2020-01-09 10:06:42 -0400

    Creating Split Window

--- RedditUWPClient/App.xaml.cs
+++ RedditUWPClient/App.xaml.cs
@@ -66,7 +66,7 @@ namespace RedditUWPClient
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(MainSplitted), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
